5f734927383515723143104fcc7c56f605ab99ab
nirmal56 <nirmal.patel@einfochips.com>
2022-07-11 18:09:27 +0530

    added calculator

--- ifthen.sh
+++ ifthen.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 count=100
+num=1
 if [ $count -eq 100 ]
 then
     echo byr bye
-# if [ $count+1 -eq 101 ]
-# then
-#     echo hihi
+elif [ $(($count+$num)) -eq 101 ]
+then
+    echo hihi
 else
     echo nothing
 fi
